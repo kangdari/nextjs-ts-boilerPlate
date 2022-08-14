93b048ad8bdca4974bdfa338e23dfa5e7958f81a
KangSangHyeok <ksh44820@naver.com>
2022-08-14 22:35:18 +0900

    done: react-query 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,24 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { Hydrate } from "react-query/hydration";
+import React from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const queryClientRef = React.useRef<QueryClient>();
+  if (!queryClientRef.current) {
+    queryClientRef.current = new QueryClient();
+  }
+
   return (
-    <>
-      <Head>
-        <title>WRT</title>
-      </Head>
-      <Component {...pageProps} />
-    </>
+    <QueryClientProvider client={queryClientRef.current}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Head>
+          <title>WRT</title>
+        </Head>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
   );
 }
 
