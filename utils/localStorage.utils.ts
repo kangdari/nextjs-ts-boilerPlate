26c830aaba5e1fefd1f5afa13ab9af3bbd34871f
KangSangHyeok <ksh44820@naver.com>
2022-08-15 14:20:39 +0900

    fix: util 함수명 수정

--- utils/localStorage.utils.ts
+++ utils/localStorage.utils.ts
@@ -1,8 +1,9 @@
-const getData = (key: string) => localStorage.getItem(key);
+export const getLocalStorageData = (key: string) => localStorage.getItem(key);
 
-const setData = (key: string, value: string) =>
+export const setLocalStorageData = (key: string, value: string) =>
   localStorage.setItem(key, value);
 
-export const getAuthToken = () => getData("TOKEN");
+export const getAuthToken = () => getLocalStorageData("TOKEN");
 
-export const setAuthToken = (token: string) => setData("TOKEN", token);
+export const setAuthToken = (token: string) =>
+  setLocalStorageData("TOKEN", token);
